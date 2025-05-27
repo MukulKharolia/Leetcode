9cd7f58684db3b32f9933962d1652d26b46f82ef
MukulKharolia <114578496+MukulKharolia@users.noreply.github.com>
2025-05-27 11:03:05 +0530

    Time: 0 ms (100%), Space: 41.2 MB (19.43%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.java
@@ -5,11 +5,9 @@ class Solution {
         for(int i=1;i<=n;i++){
             if(i%m!=0){
                 a+=i;
-                // System.out.println(i);
             }
-            if(i%m==0){
+            else{
                 b+=i;
-                // System.out.println(b);
             }
         }
         return a-b;
