2b2376efbbe18ccba2c5d664678308028fe86d57
MukulKharolia <114578496+MukulKharolia@users.noreply.github.com>
2025-08-15 17:52:36 +0530

    Time: 1 ms (23.87%), Space: 40.8 MB (77.96%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+
+        if(n==1){
+            return true;
+        }
+
+        if(n%4!=0){
+            return false;
+        }
+
+        else{
+            n=n/4;
+            return isPowerOfFour(n);
+        }
+
+        // return false;
+    }
+}
