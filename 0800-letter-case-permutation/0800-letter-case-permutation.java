3ec8934f742dd8cadf2a33aa5d5e73cf8d56bb99
MukulKharolia <114578496+MukulKharolia@users.noreply.github.com>
2025-10-02 15:04:43 +0530

    Time: 2 ms (68.1%), Space: 46 MB (7.51%) - LeetHub

---
+++ 0800-letter-case-permutation/0800-letter-case-permutation.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        StringBuilder sb=new StringBuilder();
+        List<String> arr=new ArrayList<>();
+        res(s,arr,sb,0);
+        // System.out.println(arr);
+        return arr;
+    }
+    public static void res(String s, List<String> arr, StringBuilder sb,int i){
+        if(i==s.length()){
+            arr.add(sb.toString());
+            return;
+        }
+        // Character a1=s.charAt(i);
+        // Character a2=Character.toUpperCase(s.charAt(i));
+        sb.append(Character.toLowerCase(s.charAt(i)));
+        res(s,arr,sb,i+1);
+        sb.deleteCharAt(sb.length()-1);
+        if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+            return;
+        }
+        sb.append(Character.toUpperCase(s.charAt(i)));
+        res(s,arr,sb,i+1);
+        sb.deleteCharAt(sb.length()-1);
+        //  if (Character.isLetter(s.charAt(i))) {
+        //     sb.append(Character.toUpperCase(s.charAt(i)));
+        //     res(s, arr, sb, i + 1);
+        //     sb.deleteCharAt(sb.length() - 1); // IMPORTANT: backtrack here too
+        // }
+    }
+}
